7f2a4c7463b65e44ec027fae8696dd30d33f4886
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2024-03-24 20:57:01 +0530

    integrate Apollo GraphQL Server

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const cors = require('cors')
+const {ApolloServer} = require('@apollo/server')
+const {expressMiddleware} = require('@apollo/server/express4')
 require('dotenv').config();
 const PORT =  process.env.PORT || 8002;
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
+
+async function  startServer(){
+const server = new ApolloServer({typeDefs:`
+type Query {
+  hello: String
+}
+`,resolvers:{
+    Query: {
+      hello: () => 'Hello, world!',
+    },
+  }})
+
+await server.start();
+app.use('/graphql',expressMiddleware(server)) ;
+}
+startServer();
+
+
 
 app.get('/',(req,res)=>{
     res.send('Home Page')
